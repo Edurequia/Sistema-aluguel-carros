683776b93818b0b8338876c752bb1925c3e934dd
Edurequia <edurequia@gmail.com>
2023-04-21 15:29:43 -0300

    Commit

---
+++ src/model_service/RentalService.java
@@ -0,0 +1,39 @@
+package model_service;
+
+import java.time.Duration;
+
+import entities.CarRental;
+import entities.Invoice;
+
+public class RentalService {
+	
+	private double pricePerDay;
+	private double pricePerHour;
+	
+	private TaxService taxService;
+
+	public RentalService(double pricePerDay, double pricePerHour, TaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		
+		double minutes = Duration.between(carRental.getStart(), carRental.getFinish()).toMinutes();
+		double hours = minutes / 60;
+		
+		double basicPayments;
+		if (hours <= 12.0) {
+			basicPayments = pricePerHour * Math.ceil(hours);
+		}
+		else {
+			basicPayments = pricePerDay * Math.ceil(hours / 24);
+		}
+		
+		double tax = taxService.tax(basicPayments);
+		
+		carRental.setInvoice(new Invoice(basicPayments, tax));
+	}
+	
+}
---
+++ src/model_service/TaxService.java
@@ -0,0 +1,6 @@
+package model_service;
+
+public interface TaxService {
+	
+	double tax(double amount);
+}
